e471c186fe80ef74cd9c19fd64eb6acb168d5a1f
hgagliardi <hg.gagliardi@gmail.com>
2015-12-22 20:05:42 -0300

    Primer commit general

---
+++ application/views/estructura/head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	
+	<meta charset="utf-8">
+
+	<title></title>
+
+	<!-- CSS Styles -->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/main-style.css">
+
+
+	<!-- JS Files -->
+
+	<script src="<?php echo base_url(); ?>js/jquery.min.js"></script>
+
+	<!-- Fonts -->
+
+	<link href='https://fonts.googleapis.com/css?family=Kavoon' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Orbitron:400,700,900' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
---
+++ application/views/home.php
@@ -0,0 +1,86 @@
+<header id="main-head" class="main-head">
+<div class="navbar"></div>
+</header>
+
+<section id="main-section" class="main-section">
+<div class="container">
+
+<div class="row">
+
+	<div class="col-xs-9 col-md-9">
+
+		<article class="article-box col-xs-6 col-md-6">
+
+			<div class="article-thumbnail">
+
+			</div>
+
+			<header class="article-title"><h2>Título de Prueba</h2></header>
+
+			<div class="article-text">
+				<p>
+				El año se acaba y con él se lleva las tendencias de 2015. Pero no hay que preocuparse, ya están aquí las tendencias para el diseño web para este nuevo año que comienza. Veréis que algunas se mantienen pero viene también novedades… aquí tenéis 5!
+				</p>
+			</div>
+
+		</article>
+		<article class="article-box col-xs-6 col-md-6">
+
+			<div class="article-thumbnail">
+
+			</div>
+
+			<header class="article-title"><h2>Título de Prueba</h2></header>
+
+			<div class="article-text">
+				<p>
+				El año se acaba y con él se lleva las tendencias de 2015. Pero no hay que preocuparse, ya están aquí las tendencias para el diseño web para este nuevo año que comienza. Veréis que algunas se mantienen pero viene también novedades… aquí tenéis 5!
+				</p>
+			</div>
+
+		</article>
+
+		<article class="article-box col-xs-6 col-md-6">
+
+			<div class="article-thumbnail">
+
+			</div>
+
+			<header class="article-title"><h2>Título de Prueba</h2></header>
+
+			<div class="article-text">
+				<p>
+				El año se acaba y con él se lleva las tendencias de 2015. Pero no hay que preocuparse, ya están aquí las tendencias para el diseño web para este nuevo año que comienza. Veréis que algunas se mantienen pero viene también novedades… aquí tenéis 5!
+				</p>
+			</div>
+
+		</article>
+		<article class="article-box col-xs-6 col-md-6">
+
+			<div class="article-thumbnail">
+
+			</div>
+
+			<header class="article-title"><h2>Título de Prueba</h2></header>
+
+			<div class="article-text">
+				<p>
+				El año se acaba y con él se lleva las tendencias de 2015. Pero no hay que preocuparse, ya están aquí las tendencias para el diseño web para este nuevo año que comienza. Veréis que algunas se mantienen pero viene también novedades… aquí tenéis 5!
+				</p>
+			</div>
+
+		</article>
+
+	</div>
+
+	<div class="col-xs-3 col-md-3">
+
+		<aside>Aside 1</aside>
+
+	</div>
+
+</div><!-- Div Row -->
+
+
+</div> <!-- Div Container -->
+</section><!-- Section -->
